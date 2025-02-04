ef145edae90dda9355a44fc7b969323f0ceea5a4
JIMYUNG_YU <jm_7516@naver.com>
2025-02-04 15:23:40 +0900

    240204 유지명 - 메인페이지 html 생성

---
+++ webProject/src/main/java/webProject/controller/ViewController.java
@@ -0,0 +1,14 @@
+package webProject.controller;
+
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("")
+    public String index() {
+        return "/index.html";
+    }
+}
