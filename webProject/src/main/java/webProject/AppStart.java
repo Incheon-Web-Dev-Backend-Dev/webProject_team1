54381d82898a4f71be6f90ff79df6cdb9606b5d5
JIMYUNG_YU <jm_7516@naver.com>
2025-02-04 15:02:51 +0900

    Test

--- webProject/src/main/java/webProject/AppStart.java
+++ webProject/src/main/java/webProject/AppStart.java
@@ -1,7 +1,12 @@
 package webProject;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class AppStart {
     public static void main(String[] args) {
         System.out.println("");
+        SpringApplication.run(AppStart.class);
     }
 }
