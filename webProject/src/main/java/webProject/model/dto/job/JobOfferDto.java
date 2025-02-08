3b08ee0eda04f44750ef849081e96e59feee5977
jinong <sskawd@naver.com>
2025-02-08 15:16:33 +0900

    Job(Offer/File) Entity+Dto+Repository

--- webProject/src/main/java/webProject/model/dto/job/JobOfferDto.java
+++ webProject/src/main/java/webProject/model/dto/job/JobOfferDto.java
@@ -1,4 +1,26 @@
 package webProject.model.dto.job;
 
+import lombok.*;
+import webProject.model.entity.job.JobOfferEntity;
+
+@Getter@Setter@ToString@Builder@AllArgsConstructor@NoArgsConstructor
 public class JobOfferDto {
+
+    private int jono; // 구인글 번호 PK
+    private String jotitle; // 구인글 제목
+    private String jocontent; // 구인글 내용
+    private boolean jostate; // 구인글 마감상태
+    private String joservice; // 구인하는 서비스
+    private String jocity; // 구인하는 시
+    private String jodistrict; // 구인하는 구
+
+    private int mno; // 회원번호 FK
+
+    public JobOfferEntity toEntity(){
+        return JobOfferEntity.builder().jono(this.jono)
+                .jotitle(this.jotitle).jocontent(this.jocontent)
+                .jostate(this.jostate).joservice(this.joservice)
+                .jocity(this.jocity).jodistrict(this.jodistrict)
+                .build();
+    }
 }
---
+++ webProject/src/main/java/webProject/model/entity/job/JobFileEntity.java
@@ -0,0 +1,34 @@
+package webProject.model.entity.job;
+
+import jakarta.persistence.*;
+import lombok.*;
+import webProject.model.dto.job.JobFileDto;
+import webProject.model.entity.BaseTime;
+
+@Entity
+@Table(name = "jobfile") // 구인글의 첨부파일 테이블
+@Getter@Setter@ToString@Builder@AllArgsConstructor@NoArgsConstructor
+public class JobFileEntity extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int jfno; // 구인글의 첨부파일 번호 PK
+
+    @Column(columnDefinition = "varchar(255)", nullable = false)
+    private String jfname; // 구인글의 첨부파일 이름
+
+
+    @ManyToOne
+    @JoinColumn(name = "jono")
+    @ToString.Exclude@Builder.Default
+    private JobOfferEntity jobOfferEntity; // 구인글 번호 FK
+
+
+    public JobFileDto toDto(){
+        return JobFileDto.builder()
+                .jfno(this.jfno).jfname(this.jfname)
+                .jono(jobOfferEntity.getJono()).build();
+    }
+
+
+}
