ac23d35a5964ce24a548db54a01dd60973d5792a
JIMYUNG_YU <jm_7516@naver.com>
2025-02-11 10:32:44 +0900

    0210 유지명 - request 요청서 조회, 등록 메서드 생성

--- webProject/src/main/java/webProject/service/request/RequestService.java
+++ webProject/src/main/java/webProject/service/request/RequestService.java
@@ -1,4 +1,32 @@
 package webProject.service.request;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import webProject.model.dto.request.RequestDto;
+import webProject.model.entity.request.RequestEntity;
+
+import java.util.List;
+
+@Service
 public class RequestService {
+
+    @Autowired
+    private RequestEntity requestEntity;
+
+    // 현재 로그인된 회원의 요청글 전체조회
+    public List<RequestDto> requestFindAll() {
+        return null;
+    }
+
+    // 현재 로그인된 회윈의 요청글 개별조회
+    public RequestDto requestFind(@RequestParam int reqno) {
+        return null;
+    }
+
+    // 견적 요청글 작성
+    public boolean requestPost (@RequestBody RequestDto requestDto){
+        return false;
+    }
 }
