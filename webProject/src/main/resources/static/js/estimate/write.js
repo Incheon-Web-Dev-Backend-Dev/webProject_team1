b94770d3a01c2f89dd745fbbe024c456557ce07a
Yim_JunSu <yimjunsu@gmail.com>
2025-02-14 05:34:23 +0900

    yimjunsu 0214 / 1st commit 수정 내용 : 로그인 자바 스크립트 적용(로그아웃 추후), 요청글 견적쓰기 임시버튼 추가, 등등

--- webProject/src/main/resources/static/js/estimate/write.js
+++ webProject/src/main/resources/static/js/estimate/write.js
@@ -1,8 +1,82 @@
 console.log("onEstimateWrite open!")
+
+// 이전 페이지의 URL을 가져옵니다.
+const referrerUrl = document.referrer;
+
+// URLSearchParams를 사용하여 쿼리 파라미터를 추출합니다.
+const referrerParams = new URLSearchParams(new URL(referrerUrl).search);
+
+// reqno 파라미터 값을 가져옵니다.
+const reqno = referrerParams.get('reqno');
+
+// 결과를 출력합니다.
+console.log("이전 페이지 URL : " + referrerUrl);
+console.log('이전 페이지의 reqno: ' + reqno);  // 이제 정상적으로 'reqno' 값을 출력합니다.
+
 // 1. 견적서 올리기
 const onEstimateWrite = () => {
-    // 1. input dom 가져오기..
-}
+    // 요청글 상세페이지 아직 안만들어져서 임시 주석, reqno 임의값 설정
+    //const reqno = referrerParams.get('reqno'); 
+    const reqno = 10;
+    // 1. input dom 가져오기
+    let esttitleValue = document.querySelector('.esttitleValue')
+    let estcontentValue = document.querySelector('.estcontentValue')
+    let estcashValue = document.querySelector('.estcashValue')
+    
+    if (!esttitleValue || !estcontentValue || !estcashValue) {
+      alert('입력칸이 비어있습니다.');
+      return false;
+    }
+    
+    // 2. dom value 값 가져오기
+    let esttitle = esttitleValue.value; console.log(esttitle);
+    let estcontent = estcontentValue.value; console.log(estcontent);
+    let estcash = estcashValue.value; console.log(estcash);
+
+    // *유효성 검사(모든 요소 빈칸 X)
+    if(esttitle === ''){
+      alert('견적서 제목을 입력해주세요')
+      return false;
+    } else if(estcontent === ''){
+      alert('견적서 내용을 입력해주세요')
+      return false;
+    } else if(estcash === ''){
+      alert('견적을 입력해주세요')
+      return false;
+    }
+
+    // 3. 객체화
+    const estdto = {
+          esttitle : esttitle,
+          estcontent : estcontent,
+          estcash : estcash,
+          reqno : reqno
+        }
+
+    // 4. fetch
+    const option = {
+      method : 'POST',
+      headers : { 'Content-Type' : 'application/json' },
+      body : JSON.stringify(estdto)
+    }
+    fetch('/estimate/write.do' , option)
+    .then(r=>r.json())
+    .then(data => {
+      console.log(data)
+      if(data == true){
+        confirm('견적을 등록 하시겠습니까?')
+        alert("견적서 업로드 성공!")
+        location.href='/'; // 해당 요청서로 이동하기 추후 수정 
+      }else{
+        alert("요청서 업로드에 실패했습니다.")
+      }
+    })
+    .catch(e=>{console.log(e)})
+  }
+
+
+
+
 
 //========================================== 참고 효과 js =======================================================//
 const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
