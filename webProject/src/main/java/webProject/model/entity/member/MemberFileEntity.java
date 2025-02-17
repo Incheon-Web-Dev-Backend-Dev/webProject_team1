d9359a1981299eae4af1b0b001f98d122528caa3
GeunHo97 <rmsgh501@gmail.com>
2025-02-17 19:06:37 +0900

    mypage delete 추가

--- webProject/src/main/java/webProject/model/entity/member/MemberFileEntity.java
+++ webProject/src/main/java/webProject/model/entity/member/MemberFileEntity.java
@@ -21,19 +21,11 @@ public class MemberFileEntity extends BaseTime {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Unsigned
     private int mfno; //가입파일번호
-<<<<<<< HEAD
-
     @Column(columnDefinition = "varchar(255)")
     private String mfname; //파일이름
     @Column(columnDefinition = "varchar(255)")
     private String profilename; //파일이름
 
-=======
-    @Column(columnDefinition = "varchar(255)")
-    private String mfname; //첨부파일이름
-    @Column(columnDefinition = "varchar(255)")
-    private String mprofilename; //프로필이름
->>>>>>> 9de46a957f09dd0b0d8e717d55ecba7a09b9d9b4
     @ManyToOne  // FK
     @JoinColumn(name = "mno")
     private MemberEntity memberEntity;
@@ -42,14 +34,8 @@ public class MemberFileEntity extends BaseTime {
         return MemberFileDto.builder()
                 .mfno(this.mfno)
                 .mfname(this.mfname)
-<<<<<<< HEAD
-                .mfname2(this.profilename)
-=======
-                .profile(this.mprofilename)
->>>>>>> 9de46a957f09dd0b0d8e717d55ecba7a09b9d9b4
+                .profile(this.profilename)
                 .cdate(this.getCdate().toString())
                 .build();
     }
 }
-a3438dcf-158f-467f-b915-1980f4e5694c-mainbanner3.jpg
-6fcd4b2d-84b4-4902-b132-a0c879e4fd94-default.jpg
