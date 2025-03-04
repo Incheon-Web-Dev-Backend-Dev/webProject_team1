eb26ef56c602d78a4dd4d459507ea1b6af50c630
JIMYUNG_YU <jm_7516@naver.com>
2025-03-05 01:28:55 +0900

    0305 유지명 - (back) review.write 페이지 url로 연결하는 QR 코드 생성 메서드 추가 + gradle 의존성 추가

--- webProject/src/main/java/webProject/controller/review/ReviewController.java
+++ webProject/src/main/java/webProject/controller/review/ReviewController.java
@@ -1,11 +1,20 @@
 package webProject.controller.review;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import webProject.model.dto.review.ReviewDto;
 import webProject.service.review.ReviewService;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/review")
@@ -31,5 +40,31 @@ public class ReviewController {
             // 서버 내부 오류 : 500 상태코드에 대한 반환 내용
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("review.write status error" + e.getMessage());
         }// try-catch end
+    }
+
+    // QR code 받기
+    @GetMapping("/qr")
+    public ResponseEntity<byte[]> qrToReview() throws WriterException, IOException {
+
+        // QR 정보
+        int width = 200;
+        int height = 200;
+        String url = "http://localhost:8080/review/write";
+
+        // QR Code - BitMatrix : qr code 정보 생성
+        BitMatrix encode = new MultiFormatWriter().encode(url, BarcodeFormat.QR_CODE, width, height);
+
+        // QR Code 이미지 생성 (1회성)
+        // stream으로 Generate(1회성이 아니면 file로 작성?)
+        try{
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            MatrixToImageWriter.writeToStream(encode, "PNG", out);
+
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(out.toByteArray());
+        }catch (Exception  e) {
+
+            System.out.println("review qr error" + e.getMessage());
+        }
+        return null;
     }
 }
