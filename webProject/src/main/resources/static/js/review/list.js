f2793bb88c772f547e5aec57c4e4a50adc305699
JIMYUNG_YU <jm_7516@naver.com>
2025-03-13 23:25:56 +0900

    0313 유지명 - Review.list. 수정 중

--- webProject/src/main/resources/static/js/review/list.js
+++ webProject/src/main/resources/static/js/review/list.js
@@ -17,28 +17,32 @@ const reviewFindAll = () => {
                 if(list.revno > 0) { // 리뷰가 존재하면 보여주기
                     // 1) 리뷰데이터에 이미지가 있으면 첫번째 이미지 하나만 추출하기
                     let reviewImg = '';
-                    if(list.revimgList && list.revimgList.length >0) { // 이미자가 존재하면
+                    if(list.revimgList && list.revimgList.length >0) { // 이미지가 존재하면
                         reviewImg = list.revimgList[0];
                     }
+                    console.log("리뷰이미지 파일명" + reviewImg);
+                    
 
                     // 2) 별점에 따른 아이콘 생성
                     let starHtml = '';
                     for(let i = 0; i < list.revstar; i++) {
-                        starHtml += '<i>';
+                        starHtml += `<i class="fa-solid fa-star"></i>`;
                     }
                     for(let i = list.revstar; i < 5; i++) {
-                        starHtml += '☆';
+                        starHtml += `<i class="fa-regular fa-star"></i>`;
                     }
 
                     html += `
                         <div class="card" style="width: 16rem;">
-                            ${reviewImg ? `<img src="${reviewImg}" class="card-img-top revimgList" alt="리뷰이미지">` : ''}
+                            ${reviewImg ? 
+                                `<img src="${reviewImg.includes('http') ? reviewImg : '/img/review/' + reviewImg}" class="card-img-top revimgList" alt="리뷰이미지"">` : 
+                                `<img src="/img/WebProjectLogo.png" class="card-img-top revimgList" alt="기본 이미지">`}
                             <div class="card-body">
-                                <p class="card-text revstar">${list.revstar}</p>
-                                <p class="card-text revcontent"><a href="/review/view?revno=${list.revno}>${list.revcontent}</a></p>
+                                <p class="card-text revstar">${starHtml}</p>
+                                <p class="card-text revcontent"><a href="/review/view?revno=${list.revno}">${list.revcontent}</a></p>
                             </div>
                         </div>
-                    `
+                    `;
                 }// if end
             })// foreach end
             cardBox.innerHTML = html;
@@ -47,8 +51,3 @@ const reviewFindAll = () => {
 } // reviewFindAll end
 
 reviewFindAll();
-
-// 리뷰 별점 숫자에 따른 아이콘 출력 기능
-const makeStar = () => {
-
-}
